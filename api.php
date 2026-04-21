46f282bbc808b24aef56e328374c2225172781f4
SafsGam <weithe.ja@outlook.de>
2026-04-21 09:31:02 +0200

    löschen vom master

--- api.php
+++
@@ -1,8 +0,0 @@
-<?php
-    header("Content-Type: application/json");
-    echo json_encode([
-        ["id" => 1, "title" => "Aufgabe 1", "completed" => false],
-        ["id" => 2, "title" => "Aufgabe 2", "completed" => true],
-        ["id" => 3, "title" => "Aufgabe 3", "completed" => false]
-    ]);
-?>
