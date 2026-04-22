1980cf92503eb2dba631418e88edd48fe9255c0a
Teodor Pesic <teodor.pesic1709@gmail.com>
2026-04-22 20:48:52 +0200

    tasks hinzugefügt

--- tasks.php
+++ tasks.php
@@ -3,6 +3,9 @@
 header("Content-type: application/json");
 
 echo json_encode([
-    ["id" =>1, "title" => "Erste Aufgabe"],
-    ["id" =>2, "title" => "Zweite Aufgabe"]
+    ["id" =>1, "title" => "php api erstellen"],
+    ["id" =>2, "title" => "html seite erstellen"],
+    ["id" =>3, "title" => "css styling hinzufügen"],
+    ["id" =>4, "title" => "git branch erstellen"],
+    ["id" =>5, "title" => "merge request"]
 ]);
